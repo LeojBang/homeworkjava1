8778224b7de2c14e04b23d1966a194f15eb4175f
maksim <m.merkulovdodo@gmail.com>
2024-08-06 17:23:14 +0300

    Done first homework java. Full 8 tasks

--- src/Main.java
+++ src/Main.java
@@ -54,5 +54,94 @@ public class Main {
         totalPer = totalPer + addPer;
         totalHours = totalPer * 8;
         System.out.println(" Если в этой компании работает " + totalPer + " человек, то всего " + totalHours + " часов работы может быть поделено между сотрудниками ");
+
+        byte a = 5;
+        short b = 10;
+        int c = 15;
+        long d = 20L;
+        float e = 1.5234f;
+        double f = 3.554543;
+        System.out.println(" Значение переменной a с типом byte равно " + a);
+        System.out.println(" Значение переменной b с типом short равно " + b);
+        System.out.println(" Значение переменной c с типом int равно " + c);
+        System.out.println(" Значение переменной d с типом long равно " + d);
+        System.out.println(" Значение переменной e с типом float равно " + e);
+        System.out.println(" Значение переменной f с типом double равно " + f);
+
+       // float z = 27.12f;
+       // long x = 987678965549L;
+       // float v = 2.786f;
+       // short n = 569;
+      //  short m = -159;
+      //  short s = 27897;
+      //  byte h = 67;
+
+        byte class1 = 23;
+        byte class2 = 27;
+        byte class3 = 30;
+        short papers = 480;
+        int allClasses = (class1 + class2 + class3);
+        int paperPerStudient = papers / allClasses;
+        System.out.println(" На каждого ученика рассчитано " + paperPerStudient + " листов бумаги ");
+
+        short boatlesPerMinute = 8;
+        short boatlesPerMinutes20 = (short) (20 * boatlesPerMinute);
+        System.out.println(" За 20 минут машина произвела " + boatlesPerMinutes20 + " штук бутылок ");
+        short boatlesPerDay = (short) (boatlesPerMinute * 1440);
+        System.out.println(" За cутки машина произвела " + boatlesPerDay + " штук бутылок ");
+        int boatlesPer3Day = (boatlesPerMinute * 1440 * 3);
+        System.out.println(" За 3 cуток машина произвела " + boatlesPer3Day + " штук бутылок ");
+        int boatlesPer30Day = (boatlesPerMinute * 1440 * 30);
+        System.out.println(" За 30 cуток машина произвела " + boatlesPer30Day + " штук бутылок ");
+
+        short banokKraski = 120;
+        short whitePerRoom = 2;
+        short brownPerRoom = 4;
+        short perRoom = (short) ( whitePerRoom + brownPerRoom);
+        short rooms = (short) (banokKraski/perRoom);
+        short whiteBanok = (short) (rooms * whitePerRoom);
+        short brownBanok = (short) (rooms * brownPerRoom);
+        System.out.println("В школе, где " + rooms + " классов, нужно " + whiteBanok + " банок белой краски и " + brownBanok + " банок коричневой краски.");
+
+        byte weightBanana = 80;
+        short milkWeight = 100;
+        byte iceCream = 100;
+        byte egg = 70;
+        double breakestWeight = (weightBanana * 5) + (milkWeight * 1.05 * 2) + (iceCream * 2) + (egg * 4);
+        System.out.println("Вес завтрака в граммах равен " + breakestWeight);
+        breakestWeight = breakestWeight/1000;
+        System.out.println("Вес завтрака в килограммах равен " + breakestWeight);
+
+        short deficit1 = 250;
+        short deficit2 = 500;
+        byte goal = 7;
+        short days1 = (short) (goal*1000/deficit1);
+        short days2 = (short) (goal*1000/deficit2);
+        short avgDays = (short) ((short) (goal*1000)/((deficit1+deficit2)/2));
+        System.out.println(" Дней, чтобы похудеть при 250 граммах в неделю на 7 кг " + days1);
+        System.out.println(" Дней, чтобы похудеть при 500 граммах в неделю на 7 кг " + days2);
+        System.out.println(" Дней, чтобы похудеть в среднем на 7 кг " + avgDays);
+
+        int paydayMasha = 67760;
+        int paydayDenis = 83690;
+        int paydayChris = 76230;
+        float paydayBonus = 1.1f;
+        int paydayMashaYear = paydayMasha * 12;
+        int paydayDenisYear = paydayDenis * 12;
+        int paydayChrisYear = paydayChris * 12;
+        int paydayMashaBonus = (int) (paydayMasha * paydayBonus);
+        int paydayDenisBonus = (int) (paydayDenis * paydayBonus);
+        int paydayChrisBonus = (int) (paydayChris * paydayBonus);
+        int paydayMashaBonusYear = paydayMashaBonus * 12;
+        int paydayDenisBonusYear = paydayDenisBonus * 12;
+        int paydayChrisBonusYear = paydayChrisBonus * 12;
+        int paydayDiffMasha = paydayMashaBonusYear - paydayMashaYear;
+        int paydayDiffDenis = paydayDenisBonusYear - paydayDenisYear;
+        int paydayDiffChris = paydayChrisBonusYear - paydayChrisYear;
+
+        System.out.println(" Маша теперь получает " + paydayMashaBonus + " Годовой доход вырос на " + paydayDiffMasha + " рублей ");
+        System.out.println(" Денис теперь получает " + paydayDenisBonus + " Годовой доход вырос на " + paydayDiffDenis + " рублей ");
+        System.out.println(" Кристина теперь получает " + paydayChrisBonus + " Годовой доход вырос на " + paydayDiffChris + " рублей ");
+
     }
 }
